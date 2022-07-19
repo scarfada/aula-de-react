32a5c31ad1b4ddb6a19e10e77a611d062fa802c4
scarfada <scarletbonfimlove@gmail.com>
2022-07-19 13:24:15 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './style.css';
+
+export const App = () => {
+  return (
+    <div>
+      Oiiii!
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-puxxhu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-puxxhu)
